66943639d52616b56b411da47669dc3f6c9023b8
ChrisKarpinski <christopherkarpinski@cmail.carleton.ca>
2017-09-25 15:19:10 -0400

    Create E2.py

---
+++ E2.py
@@ -0,0 +1,17 @@
+# Exercise 2 - Tutorial 2
+
+def isEven(number):
+    
+    
+    
+    if int(number)%2 == 0:
+        
+        print (int(number), "is even")
+    else:
+        
+        print (int(number), "is odd")
+        
+
+aNumber = input("Enter an integer: ")
+
+isEven(aNumber)
